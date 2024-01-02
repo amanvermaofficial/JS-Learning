a0d92354cb8486fe27d4beb4771b34fc771abeff
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-01-02 09:32:45 +0000

    container config done

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -12,3 +12,11 @@ while (arr<myArray.length) {
     console.log(`Value is ${myArray[arr]}`);
     arr=arr+1;
 }
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
