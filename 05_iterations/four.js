2ca0649685af5f5242c9a3225fba76cba7c1b57f
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-01-02 16:52:03 +0000

    container config done

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+myObj ={
+    cpp:"C++",
+    py:"Python",
+    js: 'javascript',
+    rb:"Ruby"
+}
+
+for (const key in myObj) {
+//    console.log(`${key} is shortcut for ${myObj[key]}`);
+}
+
+
+const programming = ["js","rb","py","java"]
+
+for (const key in programming) {
+//  console.log(key);
+// console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// } 
+
+//in maps we can't iterations on map
