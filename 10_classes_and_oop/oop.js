5998cb1560079990c838c7898765ecdf069f9420
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-01-17 18:09:44 +0000

    container condig done

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,40 @@
+const user = {
+    username:"hitesh",
+    loginCount:8,
+    signedIn: true,
+
+    getUserDetails: function(){
+             //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username,loginCount,isLoggedIn){
+    this.username=username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+
+    return this
+
+}
+
+const userOne =  User("hitesh", 12, true)
+// const userTwo = new User("ChaiAurCode", 11, false)
+
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+// new =>when we use 'new' keyword first of all crete empty object also known as instance
+//step 2=>constructor function called cause of new keyword
+//step 3=>this keyword inject all arguments which we write
+//step 4=>we gets
