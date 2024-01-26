57b36560e1d23204245363f448896c842a742aa2
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-01-26 14:49:15 +0000

    container config done

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,23 @@
+class User{
+    constructor(email,password){
+        this.email=email;
+        this.password=password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+       this._email=value
+    }
+    get password(){
+        return `${this._password}aman`
+    }
+    set password(value){
+       this._password=value
+    }
+}
+
+const hitesh = new User("aman@gmail.com","123")
+console.log(hitesh.email)
+console.log(hitesh.password)
