365b19141aee7cb97ca638cd91ad7be381803c90
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-03-15 00:29:21 +0000

    container config done

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,24 @@
+//kis trh memory mein data ko rakha jaata hai or access kiya jaata hai-primitive or non - primitive
+
+//primitive - it call by value-jab bhi copy krte hai memory ka refrence nhi diya jaata h balki call by value di jaati h
+
+//7 types: String ,Number ,Boolean,null,undefined,symbol(kisi bhi value ko unique banane ke liy),bigInt
+
+//Regfrence(Non primitive)
+
+//Array,object,function
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
