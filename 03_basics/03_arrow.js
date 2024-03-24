eafc7ea5a99c328bc4e8a30cdb735e66cd5d5805
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-03-24 11:11:17 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -18,20 +18,23 @@ user.username = "sam"
 // function chai(){
 //     let username = "hitesh"
 //     console.log(this.username)
-// }
+// }//this.username in function is undefined
 
 // chai();
 const chai = () => {
     let username = "hitesh";
     console.log(this.username);
-}
+}//here laso this is not work
 // chai();
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+//{} => return use hoga or () use nhi hoga return
+
+
+// const addTwo = (num1, num2) =>  num1 + num2 //implicit return
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
