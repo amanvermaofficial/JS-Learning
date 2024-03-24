729eeb4458233b4a22e44d078816d6ff679188a1
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-03-24 11:20:24 +0530

    Update 04_Iife.js

--- 03_basics/04_Iife.js
+++ 03_basics/04_Iife.js
@@ -5,6 +5,10 @@
     console.log(`DB Conected`);
 })();
 
+
+//()()=>first() is function defination and 2nd execution call
+
+//interview=>jo function immediately execute ho jaye or global scope ke pollution  se problem hoti hai kae baar ,global scope ke jo variables or jo bhi declartion hai uske pollution ko htane ke liy humne IfEE ka use kiya
 ((name)=>{
     //unnamed iffe
 console.log(`DB Connected two ${name}`);
