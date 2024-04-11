1459bba0145be4eed50314fdabed1a6d357f02ca
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-04-11 08:43:27 +0530

    Update call.js

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -1,12 +1,13 @@
 function setUsername(username){
     //complex DB class
     this.username =username
-    console.log("called");
+    console.log(this.username);
     }
     
     function createUser(username,email,password){
-        setUsername.call(this,username)
-    
+        setUsername.call(this,username)//refrence hold krne ke liy method hota hai call and call hamra current execution context kisi or function ko pass krdet hai 
+    //setusername call to ho rha hai but stack ke se jab vh execution hokar pop hota hai to vo apne variables ko saath lekar pop hota hai lekin jab hum setusername.call(this,username) tab setusername khta hai mein to jaa is execution se but aap mere properties ko rkh lo use krlena
+   
         this.email=email;
         this.password=password;
     }
