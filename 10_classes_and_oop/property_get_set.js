b5cc532e2228777c534017bbf062edbed1ec4d83
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-04-14 21:53:41 +0530

    Update property_get_set.js

--- 10_classes_and_oop/property_get_set.js
+++ 10_classes_and_oop/property_get_set.js
@@ -1,17 +1,26 @@
 function User(email,password){
-    this.email=email;
-    this.password=password
+    this._email=email;
+    this._password=password
 
-    Object.defineProperty(this, '_el',{
+    Object.defineProperty(this, 'password',{
         get: function(){
-            return this.password.toUpperCase();
+            return this._password.toUpperCase();
         },
         set: function(value){
-          this.password=value
+          this._password=value
+        }
+    })
+
+    Object.defineProperty(this, 'email',{
+        get: function(){
+            return this._email.toUpperCase();
+        },
+        set: function(value){
+          this._email=value
         }
     })
     
 }
-const U = new User("xyz@mail.com","123***")
+const U = new User("xyz@mail.com","abc123")
 
-console.log(U._el)
+console.log(U.email)
