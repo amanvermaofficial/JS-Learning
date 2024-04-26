4744eee8e3f7ba20fab458e4c6a50e116cf4a241
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-04-26 15:23:29 +0530

    Update array1.js

--- 02_basics/array1.js
+++ 02_basics/array1.js
@@ -1,4 +1,6 @@
 //arrayy
+// array-copy operations create shallow copies.  shallow copy of an object is a copy whose properties share the same references
+// it means if we make any changes it will be made changes on original 
 
 const myArr = [0,1,2,3,7,5];
 const myHeros=["shaktiman","hello"]
@@ -17,12 +19,17 @@ const newArr = myArr.join();//conversion arr to str
 
 
 //slice,splice 
-console.log("A ",myArr);
-const  myn1 = myArr.slice(1,3);
-console.log(myn1);
-console.log("B ",myArr);
-const  myn2 = myArr.splice(1,2);
-console.log(myn2);
-console.log("C ",myArr);
+slice, splice (Methods) */
+console.log("A ", myArr); // Output: A  [ 0, 1, 2, 3, 4 ]
+const myNewArr1 = myArr.slice(1, 3) /* Include value at index 1 and 2 but don't 
+include value at index 3. */
+console.log(myNewArr1); // Output: [ 1, 2 ]
+console.log("B ", myArr); // Output: B  [ 0, 1, 2, 3, 4 ]
+// Note: As we can see using slice doesn't affect original array.
+
+const myNewArr2 = myArr.splice(1, 3)
+console.log(myNewArr2); // Output: [ 1, 2, 3 ]
+console.log("C ", myArr); // Output: C  [ 0, 4 ]
+// Note: But in splice we can see it removes the elements and manipulates the original array.
 
 
