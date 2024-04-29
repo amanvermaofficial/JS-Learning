e09d2a6aed191b16ce02f1fba382a33b2dee10a7
Aman <146328707+amanvermaofficial@users.noreply.github.com>
2024-04-29 11:19:28 +0530

    Update conversionOperation.js

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -3,8 +3,8 @@ let score = "hitesh"
 //console.log(typeof score);
 //console.log(typeof(score));
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//NaN
 
 // "33" => 33
 // "33abc" => NaN
